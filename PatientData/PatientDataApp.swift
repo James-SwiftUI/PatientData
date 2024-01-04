70c4210badb5634ab94788fab1f9dc47afd8a605
James-SwiftUI <jamesholmson@gmail.com>
2024-01-04 14:45:31 +0000

    Initial Commit

---
+++ PatientData/PatientDataApp.swift
@@ -0,0 +1,17 @@
+//
+//  PatientDataApp.swift
+//  PatientData
+//
+//  Created by James on 04/01/2024.
+//
+
+import SwiftUI
+
+@main
+struct PatientDataApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
